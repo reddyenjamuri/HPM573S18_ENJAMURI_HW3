20027cedc6f110afc87dbddcb62e0e7e8da06f70
reddyenjamuri <reddy.enjamuri@yale.edu>
2018-02-06 23:17:19 -0500

    FINAL_HPM573S18_ENJAMURI_HW3

--- Question 1.py
+++ Question 1.py
@@ -18,8 +18,11 @@ class HospitalizedPatient(Patient):
         print("Hosptialized patient")
 
 class Hospital(Patient):
-    def __init__(self, cost, discharge, get_total_cost):
-        Patient.__init__(self, cost):
+    def __init__(self, patients, cost, admit, discharge, get_total_cost):
+        Patient.__init__(self, patients, cost):
+
+        def patients(self):
+            """storing the patient"""
 
         def cost(self):
             if HospitalizedPatient:
@@ -27,6 +30,8 @@ class Hospital(Patient):
             elif EmergencyPatient:
                 exp_cost = 1000
             return exp_cost
+
+        def admit(self):
 
         def discharge(self):
 
@@ -38,31 +43,3 @@ class Hospital(Patient):
             total_cost += exp_cost[i]
             i+=1
             return exp_cost
-
-            def get_expected_cost(self):
-        """
-        :return: expected cost of this chance node
-        """
-        exp_cost = self.cost  # expected cost initialized with the cost of visiting the current node
-        i = 0
-        for node in self.futureNodes:
-            exp_cost += self.probs[i]*node.get_expected_cost()
-            i += 1
-        return exp_cost
-
-
-def get_expected_costs(self):
-
-         exp_cost = self.costs
-         exp_cost = 0 # the expected cost of this node
-         i = 0 #index to iterate over probabilities
-         for thisNode in self.future_nodes:
-             if type(thisNode) == ChanceNode:
-                exp_cost += thisNode.get_expected_cost() * self.probs[i]
-            elif type(thisNode) == TerminalNode:
-                exp_cost += thisNode.get_cost*() * self.probs [i]
-
-        num_outcomes = len(self.probs)
-        expected_costs = 0
-        for i in range(num_outcomes):
-        expected_costs += self.probs[i] * self.costs[i]
